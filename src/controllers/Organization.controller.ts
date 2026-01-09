41c4548d460e1759978a683099dd9786fbb57f73
Urjashee <urja@simpalm.com>
2026-01-09 15:38:03 +0530

    service return details on add edit

--- src/controllers/Organization.controller.ts
+++ src/controllers/Organization.controller.ts
@@ -74,12 +74,12 @@ export class AuthController {
                 const settings = await this.organizationService.editServiceDetails(req.body.id, req.user.organization_id, req.user.role, req.body, null, getOrganization)
                 if (!settings)
                     return ResponseFormatter.errorResponse(res, "Can't edit, try again later");
-                return ResponseFormatter.successResponse(res, "Successfully updated service.");
+                return ResponseFormatter.successResponse(res, "Successfully updated service.", settings);
             } else {
                 const settings = await this.organizationService.addServiceDetails(req.user.organization_id, req.user.role, req.body, req.user.id, getOrganization)
                 if (!settings)
                     return ResponseFormatter.errorResponse(res, "Can't add, try again later");
-                return ResponseFormatter.successResponse(res, "Successfully added service.");
+                return ResponseFormatter.successResponse(res, "Successfully added service.", settings);
             }
         } catch (error: any) {
             return ResponseFormatter.errorResponse(res, error.message || 'An error occurred');
