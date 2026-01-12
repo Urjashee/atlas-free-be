32d8665b1255c39bb03a09e1158cffef46086419
Urjashee <urja@simpalm.com>
2026-01-12 15:08:37 +0530

    removed preferred language

--- src/schema/Client.schema.ts
+++ src/schema/Client.schema.ts
@@ -8,7 +8,7 @@ export const clientSchema = Joi.object({
     zipcode: Joi.string().min(5).max(10),
     dob: Joi.date(),
     english_speaking_ability: Joi.number(),
-    preferred_language: Joi.string(),
+    // preferred_language: Joi.string(),
     gender: Joi.array().items(Joi.number()),
     race: Joi.array().items(Joi.number()),
     citizenship_status: Joi.number(),
