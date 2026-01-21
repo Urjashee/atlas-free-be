8f7c06ed2cb2a9339f192c267cfffaa20b7f9791
Urjashee <urja@simpalm.com>
2026-01-21 14:38:33 +0530

    ein is details

--- src/util/Organization.util.ts
+++ src/util/Organization.util.ts
@@ -151,6 +151,7 @@ export async function getOrganizationsDetails(organization: any, filter?: string
         advocate: await organizationService.countOrganizationAdvocate(organization.organization.id),
         services: await organizationService.countOrganizationServices(organization.organization.id),
         tax_status: organization.organization.tax_exemption == false ? "No" : "Yes",
+        ein: organization.organization.ein,
         affiliation: role == Constants.ROLE_ADMIN ? (organization.organization.affiliations
             .filter((item: any) => item.is_active === false)
             .map((item: any) => {
