d7d330356630bf06a33ade63ac78a340d012080a
Urjashee <urja@simpalm.com>
2026-01-27 19:23:34 +0530

    children_to_accompany: Joi.number().optional().empty()

--- src/schema/Client.schema.ts
+++ src/schema/Client.schema.ts
@@ -22,7 +22,7 @@ export const clientSchema = Joi.object({
         }).allow(null).empty(""),
     birthdate_status: Joi.number().allow(null).empty("").optional(),
     children_accompany: Joi.number().optional(),
-    children_to_accompany: Joi.number().optional(),
+    children_to_accompany: Joi.number().optional().empty(""),
     criteria: Joi.array().items(Joi.number()).optional(),
     criteria_add: Joi.array().items(Joi.number()).optional(),
     medications: Joi.array().items(Joi.number()),
