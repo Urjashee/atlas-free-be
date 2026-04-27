2826761123245a8ee3e3eb79c12c15db8993b704
Urjashee <urja@simpalm.com>
2026-04-27 20:20:13 +0530

    isOrgAddress true in edit

--- src/services/Organization.service.ts
+++ src/services/Organization.service.ts
@@ -237,7 +237,7 @@ export class OrganizationService {
             city: isOrgAddress ? checkIfValidOrganization.city : body.city,
             zipcode: isOrgAddress ? checkIfValidOrganization.zipcode : body.zipcode,
             disclose_address: body.disclose_address === true || body.disclose_address === 'true',
-            is_organization_address: !isOrgAddress ? false : body.is_organization_address === true || body.is_organization_address === 'true',
+            is_organization_address: isOrgAddress ? false : body.is_organization_address === true || body.is_organization_address === 'true',
             service_type: body.service_type || null,
             total_available_slots: body.total_available_slots || null,
             slots_available: body.slots_available || body.total_available_slots,
@@ -332,7 +332,7 @@ export class OrganizationService {
                 ? checkIfValidOrganization?.zipcode
                 : body.zipcode;
             getService.disclose_address = body.disclose_address === true || body.disclose_address === 'true';
-            getService.is_organization_address = body.is_organization_address === true || body.is_organization_address === 'true';
+            getService.is_organization_address = isOrgAddress ? false : body.is_organization_address === true || body.is_organization_address === 'true';
             getService.service_type = body.service_type || null
             getService.total_available_slots = body.total_available_slots || null
             getService.slots_beds = body.slots_beds || null
